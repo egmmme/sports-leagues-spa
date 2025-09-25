5f36fd17f749fa029bf1b7dc3d2b1e195456a170
EGM <egmadruga@gmail.com>
2025-09-25 12:56:39 +0200

    format and clean up

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,24 +1,24 @@
 import * as React from "react";
-import { type VariantProps } from "class-variance-authority";
-import { cn } from "./utils";
-import { badgeVariants } from "./constants";
+import {type VariantProps} from "class-variance-authority";
+import {cn} from "./utils";
+import {badgeVariants} from "./constants";
 
 function Badge({
-  className,
-  variant,
-  asChild = false,
-  ...props
-}: React.ComponentProps<"span"> &
-  VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
-  const Comp =  "span";
+                   className,
+                   variant,
+                   asChild = false,
+                   ...props
+               }: React.ComponentProps<"span"> &
+    VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
+    const Comp = "span";
 
-  return (
-    <Comp
-      data-slot="badge"
-      className={cn(badgeVariants({ variant }), className)}
-      {...props}
-    />
-  );
+    return (
+        <Comp
+            data-slot="badge"
+            className={cn(badgeVariants({variant}), className)}
+            {...props}
+        />
+    );
 }
 
-export { Badge, badgeVariants };
+export {Badge, badgeVariants};
